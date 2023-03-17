781035d85bd5e0fd7bb8c7c66437bb4ced50a224
Kakhi Mtchedluri <kmchedluri@gmail.com>
2023-03-17 23:47:49 +0400

    build and deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -58,7 +58,7 @@ function App() {
         // Save user to MongoDB
       if(profileObj){
         const response = await fetch(
-          "http://localhost:8080/api/v1/users",
+          "https://dashboard-huyi.onrender.com/api/v1/users",
           {
               method: "POST",
               headers: { "Content-Type": "application/json" },
@@ -131,7 +131,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://dashboard-huyi.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
